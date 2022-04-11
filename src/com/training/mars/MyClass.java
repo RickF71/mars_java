f2189fc68eb1b7b196f6afd4b998f6f8a756e895
Richard Fleischman <rickf71@gmail.com>
2022-04-11 15:17:03 -0500

    Initial commit

--- src/com/training/mars/MyClass.java
+++ src/com/training/mars/MyClass.java
@@ -4,6 +4,30 @@ public class MyClass {
 
 	public static void main (String args[]) {
 		System.out.println("Hello world, first java");
+		byte numberByte=12;
+		System.out.println("Byte is " + numberByte);
+		short shortValue = 48;
+		
+		int intValue = 89;
+		
+		byte newByte = (byte)intValue; // explicit typecasting
+		
+		int newInt = shortValue; // implicit typecasting
+		
+		int charValue = 'A';
+		
+		System.out.println("charValue A is " + charValue);
+		
+		Person p1 = new Person();
+		p1.setHeight(170);
+		p1.setWeight(150);
+		System.out.println(p1.getPersonInfo());
+		
+		Person p2 = new Person();
+		p2.setHeight(100);
+		p2.setWeight(50);
+		System.out.println(p2.getPersonInfo());
+		
 	}
 	
 }
